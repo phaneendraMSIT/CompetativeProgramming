bdeddcf37b6f1475f83779a624896c2e6299d749
phaneendraMSIT <31198542+phaneendraMSIT@users.noreply.github.com>
2018-06-27 14:15:38 +0530

    Add files via upload

---
+++ Day 3/BinarySearchTress_Balanced.java
@@ -0,0 +1,149 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.launcher.listeners.TestExecutionSummary.Failure;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+@SuppressWarnings("unused")
+public class BinarySearchTress_Balanced {
+
+    public static class BinaryTreeNode {
+
+        public int value;
+        public BinaryTreeNode left;
+        public BinaryTreeNode right;
+
+        public BinaryTreeNode(int value) {
+            this.value = value;
+        }
+
+        public BinaryTreeNode insertLeft(int leftValue) {
+            this.left = new BinaryTreeNode(leftValue);
+            return this.left;
+        }
+
+        public BinaryTreeNode insertRight(int rightValue) {
+            this.right = new BinaryTreeNode(rightValue);
+            return this.right;
+        }
+    }
+
+    public static boolean isBalanced(BinaryTreeNode treeRoot) {
+
+        // determine if the tree is superbalanced
+    	int heightL = height(treeRoot.left);
+    	int heightR = height(treeRoot.right);
+    	if(heightL == 0 || heightR == 0)
+    	{
+    		return true;
+    	}
+    	if (Math.subtractExact(heightR, heightL) <= 1)
+    	{
+    		return true;
+    	}
+        return false;
+    }
+    
+    private static int height(BinaryTreeNode x) {
+        if (x == null) return 0;
+        return 1 + Math.max(height(x.left), height(x.right));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void fullTreeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(5);
+        final BinaryTreeNode a = root.insertLeft(8);
+        final BinaryTreeNode b = root.insertRight(6);
+        a.insertLeft(1);
+        a.insertRight(2);
+        b.insertLeft(3);
+        b.insertRight(4);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    @Test
+    public void bothLeavesAtTheSameDepthTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(3);
+        root.insertLeft(4).insertLeft(1);
+        root.insertRight(2).insertRight(9);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    @Test
+    public void leafHeightsDifferByOneTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(6);
+        root.insertLeft(1);
+        root.insertRight(0).insertRight(7);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    @Test
+    public void leafHeightsDifferByTwoTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(6);
+        root.insertLeft(1);
+        root.insertRight(0).insertRight(7).insertRight(8);
+        final boolean result = isBalanced(root);
+        assertFalse(result);
+    }
+
+    @Test
+    public void bothSubTreesSuperbalancedTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(1);
+        root.insertLeft(5);
+        final BinaryTreeNode b = root.insertRight(9);
+        b.insertLeft(8).insertLeft(7);
+        b.insertRight(5);
+        final boolean result = isBalanced(root);
+        assertFalse(result);
+    }
+
+    @Test
+    public void onlyOneNodeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(1);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    @Test
+    public void treeIsLinkedListTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(1);
+        root.insertRight(2).insertRight(3).insertRight(4);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(BinarySearchTress_Balanced.class);
+        for (org.junit.runner.notification.Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
