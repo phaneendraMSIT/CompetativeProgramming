3629a40733d93b767cce06ab7bfd7662de3a024c
phaneendraMSIT <31198542+phaneendraMSIT@users.noreply.github.com>
2018-06-29 12:49:31 +0530

    Find rotation point, Inflight entertainment, Compute nth fibonacci number

---
+++ Day 5/Fibonacci_nth.py
@@ -0,0 +1,49 @@
+import unittest
+
+
+def fib(n):
+    # Compute the nth Fibonacci number
+    assert n >= 0, Exception
+    if n<=1: return n
+    return fib(n-1)+fib(n-2)
+
+
+# Tests
+
+class Test(unittest.TestCase):
+    def test_zeroth_fibonacci(self):
+        actual = fib(0)
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_first_fibonacci(self):
+        actual = fib(1)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_second_fibonacci(self):
+        actual = fib(2)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_third_fibonacci(self):
+        actual = fib(3)
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_fifth_fibonacci(self):
+        actual = fib(5)
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_tenth_fibonacci(self):
+        actual = fib(10)
+        expected = 55
+        self.assertEqual(actual, expected)
+
+    def test_negative_fibonacci(self):
+        with self.assertRaises(Exception):
+            fib(-1)
+
+
+unittest.main(verbosity=2)
